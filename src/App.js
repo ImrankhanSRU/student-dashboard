45f3e00facd19f7886b74b82797035c5db7631dc
Imran <imran.khan@mountblue.io>
2020-03-16 16:27:09 +0530

    Redux installed

--- src/App.js
+++ src/App.js
@@ -4,22 +4,10 @@ import Header from "./components/Header/Header";
 import StudentsGrid from "./components/StudentsGrid/StudentsGrid";
 import { Route, Redirect } from "react-router-dom";
 import Details from "./components/Details/Details";
-import SignIn from "./components/Sign-in/Signin";
 
 class App extends Component {
   render() {
-    let val = 0;
-    if (document.cookie.split("=").length > 1) {
-      if (document.cookie.split("=")[1].length) val = 1;
-    }
-    // if (!val) {
-    //   return (
-    //     <div>
-    //       <Route path="/login" component={SignIn} />
-    //       <Redirect from = "/" to = "login" />
-    //     </div>
-    //   );
-    // }
+
 
     return (
       <>
